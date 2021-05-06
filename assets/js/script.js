13c3faf76e3b2f45f1a16b43a7aa66e74ed9263d
gabrisca <gabrielescarparo1985@gmail.com>
2021-05-06 14:09:22 +0200

    primo commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni elemento ha due stati (checked o non checked)
+// - al click dell’elemento avviene il toggle dello status
+// - al click sulla croce si elimina l’elemento
+// - clickando sul bottone si aggiunge il contenuto dell’input text solo se è più lungo di 2 caratteri
+// - se è più corto appare un alert che scompare dopo 2 secondi
+// bonus: rendere possibile l’eliminazione dell’item solo se prima è stato checkato
+
+const app = new Vue({
+  el: "#app",
+  data: {
+
+  },
+  methods: {
+    
+  },
+})
